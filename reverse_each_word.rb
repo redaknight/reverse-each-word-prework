97a4a2d17f9265a03f16aeb82a956e5214efdeac
AL FATAH KADER <alfatah.kader@gmail.com>
2018-12-17 02:25:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 def reverse_each_word(words)
   n_array = words.split(" ")
-  r_array = n_array.each {|x| x.reverse!}
+  r_array = n_array.each {|a| a.reverse!}
   return r_array.join(" ")
 end
 
 #Using .collect: 
 
 def reverse_each_word(array)
-  array.split(" ").collect {|x| x.reverse!}.join(" ")
+  array.split(" ").collect {|a| a.reverse!}.join(" ")
 end
